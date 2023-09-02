358832760447ea4b9f97021efc63591f0ae35879
clbi88 <34342166+clbi88@users.noreply.github.com>
2023-09-02 10:24:46 +0800

    Update cn.ts0903

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -266,7 +266,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "注意：\r\n 官方网址 https://chat.aizs.eu.org , 请仔细检查!!! \r\n 除此之外的任何使用均为非法.  我们从不提供API或其他收费服务 \r\n 报错处理：点左下角设置, 1.清除所有数据 2.重置所有设置 \r\n 联系邮箱: pettubudiro@mail.com  \r\n  \r\n 公告：高级版智能助手 只开放给 *** 热心用户 *** 使用，!!!  . \r\n\r\n 有什么可以帮你的吗?",
+    BotHello: "注意：\r\n 官方网址 https://chat.aizs.eu.org , 请仔细检查!!! \r\n 除此之外的任何使用均为非法.  我们从不提供API或其他收费服务 \r\n 报错处理：点左下角设置, 1.清除所有数据 2.重置所有设置 \r\n 联系邮箱: pettubudiro@mail.com  \r\n  \r\n 公告：本站属于普通版AI智能助手，资源问题高级版AI暂时只开放给***热心用户***使用，!!!  . \r\n\r\n 有什么可以帮你的吗?",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
